ea51e28febc600c8ce3c5df3a9d55ad7e0c4c01d
Brian <wamwangimathenge@gmail.com>
2023-02-07 17:15:25 +0300

    Case Statements

--- run.rb
+++ run.rb
@@ -21,4 +21,20 @@ def do_dishes dishes
     end
 end
 
+
+def to_do_week day
+    case day
+    when "Monday"
+        "Pick up dry cleaning"
+    when "Tuesday"
+        "Buy Groceries"
+    when "Wednesday"
+        "Do laundry"
+    when "Thursday"
+        "Visit the folks"
+    else
+        "Sherehe Sheria!!!"
+    end
+end
+
 binding.pry
