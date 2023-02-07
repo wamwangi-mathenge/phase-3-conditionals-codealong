340789bb3582616dddc9bf66cc0d4019582afe63
Brian <wamwangimathenge@gmail.com>
2023-02-07 17:04:43 +0300

    If else statement

---
+++ run.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+# Conditionals
+
+def to_do day
+    if day == "Monday"
+        "Pick up dry cleaning"
+    elsif day == "Friday"
+        "Take Rose to the vet"
+    else 
+        "Clean House"
+    end
+end
+
+
+def
+
+binding.pry
